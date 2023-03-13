1e13a4f2242aad150bc3eb9e9f995d74e2a722c7
Dell <kuba-bujak>
2023-03-13 15:34:41 +0100

    Add thumbnail property

--- models/campground.js
+++ models/campground.js
@@ -6,14 +6,18 @@ const Schema = mongoose.Schema;
 
 // ---------- Campgrounds Data Schema ---------- //
 
+const ImageSchema = new Schema({
+	url: String,
+	filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function() {
+	return this.url.replace('/upload', '/upload/w_200')
+})
+
 const CampgroundSchema = new Schema({
 	title: String,
-	images: [
-		{
-			url: String,
-			filename: String
-		}
-	],
+	images: [ImageSchema],
 	price: Number,
 	description: String,
 	location: String,
